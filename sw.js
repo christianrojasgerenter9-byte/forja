5ecb424780179cc8067d901fc4e3632bd2e76020
christianrojasgerenter9-byte <christian.rojas.gerente.r9@gmail.com>
2026-09-18 23:46:29 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    Nada de Firestore ni de contenido PRO pasa por el caché.
    ============================================================ */
 
-const VERSION = 'forja-v106';
+const VERSION = 'forja-v109';
 const CACHE_SHELL = `${VERSION}-shell`;
 const CACHE_EXTERNO = `${VERSION}-externo`;
 
